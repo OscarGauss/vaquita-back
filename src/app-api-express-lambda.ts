19e5ae30b4c79e10a7b28ff4df8682cf4c15294f
OscarGauss <me@oscargauss.com>
2025-02-04 12:13:36 -0400

    chore

--- src/app-api-express-lambda.ts
+++ src/app-api-express-lambda.ts
@@ -5,18 +5,24 @@ import type { APIGatewayEvent, Context } from 'aws-lambda';
 import awsServerlessExpress from 'aws-serverless-express';
 import bodyParser from 'body-parser';
 import express from 'express';
-import { finishSetupApp, initialSetupApp } from 'helpers';
+import { initialSetupApp, log } from 'helpers';
 import { errorLoggerHandler, errorResponderHandler, failSafeHandler, notFoundResponse, setCompany } from 'middlewares';
 import { dbClient } from 'services';
+import { LogLevel } from 'types';
 
 export const handler = async (event: APIGatewayEvent, context: Context) => {
   const app = initialSetupApp();
   
   try {
-    console.log({ event });
+    log(LogLevel.INFO)('event', { event });
+    
     await dbClient.connect();
     
-    finishSetupApp(app);
+    log(LogLevel.INFO)('starting finish express set up');
+    app.use(errorLoggerHandler);
+    app.use(errorResponderHandler);
+    app.use(failSafeHandler);
+    log(LogLevel.INFO)('completed finish express set up');
     
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
@@ -28,9 +34,9 @@ export const handler = async (event: APIGatewayEvent, context: Context) => {
     app.use(errorResponderHandler);
     app.use(failSafeHandler);
     
-    app.use('/vaquita/api/v1', apiV1Router);
+    app.use('/v2/vaquita', apiV1Router);
     // @ts-ignore
-    app.use('/vaquita/api/v1/group', setCompany(), apiV1GroupRouter);
+    app.use('/v2/vaquita/group', setCompany(), apiV1GroupRouter);
     
     app.use(notFoundResponse);
   } catch (error) {
