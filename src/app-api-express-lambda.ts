d5e5dfe2a208b5e2d7328e76b7efd5f93d790b63
OscarGauss <me@oscargauss.com>
2025-02-04 13:25:00 -0400

    0.0.24

--- src/app-api-express-lambda.ts
+++ src/app-api-express-lambda.ts
@@ -1,15 +1,33 @@
 import serverlessExpress from '@codegenie/serverless-express';
+import { loggerAllRequestHandler, loggerRequestTimeHandler, responsesMiddleware } from '@juki-team/base-back';
 import apiV1GroupRouter from 'app/group/route';
 import apiV1Router from 'app/route';
 import type { APIGatewayEvent, Context } from 'aws-lambda';
 import bodyParser from 'body-parser';
+import { NODE_ENV, ORIGINS, VERSION } from 'config/settings';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
-import { initialSetupApp, log } from 'helpers';
+import { log, shouldDisplayLog } from 'helpers';
 import { errorLoggerHandler, errorResponderHandler, failSafeHandler, notFoundResponse, setCompany } from 'middlewares';
 import { dbClient } from 'services';
 import { LogLevel } from 'types';
 
-const app = initialSetupApp();
+log(LogLevel.INFO)('starting initial express set up', { NODE_ENV, VERSION, ORIGINS });
+const app = express();
+app.disable('x-powered-by');
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true }));
+if (shouldDisplayLog(LogLevel.DEBUG)) {
+  app.use(loggerAllRequestHandler);
+} else if (shouldDisplayLog(LogLevel.INFO)) {
+  app.use(loggerRequestTimeHandler);
+}
+
+app.use(responsesMiddleware);
+app.use(cors({ origin: ORIGINS, credentials: true }));
+app.use(cookieParser());
+log(LogLevel.INFO)('completed express set up');
 log(LogLevel.INFO)('starting finish express set up');
 app.use(errorLoggerHandler);
 app.use(errorResponderHandler);
--- src/config/settings.ts
+++ src/config/settings.ts
@@ -3,7 +3,6 @@ import * as process from 'process';
 export {
   NODE_ENV,
   VERSION,
-  ORIGINS,
   PORT,
   AWS_REGION,
   LOG_LEVEL,
@@ -12,6 +11,11 @@ export {
   AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY,
 } from '@juki-team/base-back';
+
+export const ORIGINS = [
+  ...(process.env.ORIGINS || '').split(','),
+  /^((https:\/\/vaquita\.fi)|(https:\/\/[a-zA-Z0-9\-_]+\.vaquita\.fi))$/,
+];
 
 export const MONGO_DATABASE_URI = process.env.MONGO_DATABASE_URI || '';
 export const MONGO_DATABASE_NAME = process.env.MONGO_DATABASE_NAME || '';
