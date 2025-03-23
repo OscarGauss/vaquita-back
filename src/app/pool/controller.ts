38ce49adb0b6407d81f304a2ef2e8ef9784ce6f0
OscarGauss <me@oscargauss.com>
2025-03-22 20:11:43 -0400

    chore

--- src/app/pool/controller.ts
+++ src/app/pool/controller.ts
@@ -1,5 +1,7 @@
+import { createPoolDeposit, getPoolDepositByDepositId, updatePoolDeposit } from 'app/pool/services';
+import { DepositPoolStatus, GroupCrypto, PoolDepositBaseDocument } from 'app/pool/types';
 import { logService } from 'services/log';
-import { ErrorCode, JkError, JkRequest, JkResponse, NextFunction } from 'types';
+import { JkRequest, JkResponse, NextFunction } from 'types';
 
 export const getAllDeposits = async (req: JkRequest, res: JkResponse, next: NextFunction) => {
   
@@ -14,8 +16,39 @@ export const postScrollTransactionsDeposit = async (req: JkRequest, res: JkRespo
     body: req.body, headers: req.headers, params: req.params,
   });
   
-  res.sendError(new JkError(ErrorCode.ERR500, { message: 'unprocessed' }));
+  const transactionHash = req.body?.event?.data?.block?.logs?.[0]?.transaction?.hash;
+  const timestamp = Number(req.body?.event?.data?.block?.timestamp ?? 0);
+  const amount = BigInt(req.body?.event?.data?.block?.logs?.[0]?.data ?? 0).toString();
+  const depositId = req.body?.event?.data?.block?.logs?.[0]?.data?.topics?.[1] ?? '';
+  const customerPublicKey = req.body?.event?.data?.block?.logs?.[0]?.transaction?.from?.address ?? '';
+  const contractAddress = req.body?.event?.data?.block?.logs?.[0]?.account?.address ?? '';
+  
+  const poolDeposit: PoolDepositBaseDocument = {
+    companyId: '',
+    transactionHash,
+    timestamp,
+    amount,
+    depositId,
+    customerPublicKey,
+    crypto: GroupCrypto.USDC,
+    status: DepositPoolStatus.ACTIVE,
+    event: req.body?.event,
+    contractAddress,
+    rewardWithdrawn: '0',
+    amountWithdrawn: '0',
+  };
+  
+  await createPoolDeposit(poolDeposit);
+  
+  res.sendContent(true);
 };
+
+function splitInHalf(str: string): [ string, string ] {
+  const mid = Math.floor(str.length / 2);
+  const firstHalf = str.slice(0, mid);
+  const secondHalf = str.slice(mid);
+  return [ firstHalf, secondHalf ];
+}
 
 export const postScrollTransactionsWithdraw = async (req: JkRequest, res: JkResponse, next: NextFunction) => {
   
@@ -23,5 +56,13 @@ export const postScrollTransactionsWithdraw = async (req: JkRequest, res: JkResp
     body: req.body, headers: req.headers, params: req.params,
   });
   
-  res.sendError(new JkError(ErrorCode.ERR500, { message: 'unprocessed' }));
+  const depositId = req.body?.event?.data?.block?.logs?.[0]?.data?.topics?.[1] ?? '';
+  const [ amount, reward ] = splitInHalf((req.body?.event?.data?.block?.logs?.[0]?.data + '').replace('0x', ''));
+  const amountWithdrawn = BigInt(`0x${amount}`).toString();
+  const rewardWithdrawn = BigInt(`0x${reward}`).toString();
+  
+  const poolDeposit = await getPoolDepositByDepositId('', depositId);
+  await updatePoolDeposit(poolDeposit._id.toString(), { amountWithdrawn, rewardWithdrawn });
+  
+  res.sendContent(true);
 };
--- src/app/pool/services.ts
+++ src/app/pool/services.ts
@@ -1,35 +1,32 @@
 import { Filter, Sort } from 'mongodb';
 import { dbCrud } from 'services/database';
-import { log } from 'services/log';
-import { CreateEntityDocument, ErrorCode, JkError, LogLevel, UpdateEntityDocument } from 'types';
-import { GroupBaseDocument, GroupDocument } from './types';
+import { CreateEntityDocument, UpdateEntityDocument } from 'types';
+import { PoolDepositBaseDocument, PoolDepositDocument } from './types';
 
-const { findOne, insertOne, deleteOne, updateOne, findByFilter } = dbCrud<GroupBaseDocument>('pool-deposit');
+const {
+  insertOne,
+  updateOne,
+  findByFilter,
+  findOneByFilter,
+} = dbCrud<PoolDepositBaseDocument>('pool-deposit');
 
-export const getGroups = async (
+export const getPoolDeposits = async (
   companyId: string,
-  filter: Filter<GroupDocument>,
+  filter: Filter<PoolDepositDocument>,
   sort?: Sort,
-): Promise<GroupDocument[]> => findByFilter({ ...filter, companyId }, { sort });
+): Promise<PoolDepositDocument[]> => findByFilter({ ...filter, companyId }, { sort });
 
-export const createGroup = async (
-  contest: CreateEntityDocument<GroupBaseDocument>,
+export const createPoolDeposit = async (
+  poolDeposit: CreateEntityDocument<PoolDepositBaseDocument>,
 ) => {
-  return await insertOne(null, null, contest);
+  return await insertOne(null, null, poolDeposit);
 };
 
-export const getGroup = async (companyId: string, id: string): Promise<GroupDocument> => {
-  const group = await findOne(id);
-  if (group.companyId !== companyId) {
-    log(LogLevel.INFO)(JSON.stringify({ groupId: id, companyId }), 'error on getGroupData');
-    throw new JkError(ErrorCode.ERR0211, { message: 'not found' });
-  }
-  return group;
+export const getPoolDepositByDepositId = async (companyId: string, depositId: string): Promise<PoolDepositDocument> => {
+  return await findOneByFilter({ depositId, companyId });
 };
 
-export const updateGroup = async (
+export const updatePoolDeposit = async (
   id: string,
-  doc: UpdateEntityDocument<GroupDocument>,
+  doc: UpdateEntityDocument<PoolDepositBaseDocument>,
 ) => updateOne(id, doc, null);
-
-export const deleteGroup = async (id: string) => deleteOne(id, null);
